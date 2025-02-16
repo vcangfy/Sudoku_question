9cdaf0a78f10a9a3e4e2aad0ba6557578b375e2b
Sunwenjie <941571283@qq.com>
2025-02-16 11:11:07 +0800

    finish PaiChu, now it can finish middle Sudoku

--- Sudoku.h
+++ Sudoku.h
@@ -11,6 +11,16 @@
 #define SUDOKU_H
 using namespace std;
 
+namespace std {
+    template<> struct hash<std::array<uint32_t, 2>> {
+        size_t operator()(const std::array<uint32_t, 2>& arr) const {
+            size_t hash = 0;
+            hash = (size_t)arr[0] << 32 + (size_t)arr[1];
+            return hash;
+        }
+    };
+}
+
 //只要用了模板，哪怕是stl的容器，原型和定义就不能分开
 typedef array<uint32_t, 2> position;
 template <typename T>
@@ -22,27 +32,26 @@ class Sudoku
     chess<unordered_set<int>> lattices_front;
     chess<unordered_set<int>> lattices_back;
 
+    vector<position> sure;
+    unordered_set<position> not_sure;
+    uint32_t remember_sure;
+
     //array<array<unordered_set<uint32_t>, 9>, 3> structrue;
 
-    void start_state_to_lattices_front() {
+    void start_state() {
+        remember_sure = 0;
         for (uint32_t i = 0; i < 9; ++i) {
             for (uint32_t j = 0; j < 9; ++j) {
-                if (Chessboard[i][j] == 0)
+                if (Chessboard[i][j] == 0) {
                     lattices_front[i][j] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-                else
-                lattices_front[i][j] = {(Chessboard[i][j])};
-            }
-        }
-    };
-
-    void start_state_to_lattices_back() {
-        for (uint32_t i = 0; i < 9; ++i) {
-            for (uint32_t j = 0; j < 9; ++j) {
-                if (Chessboard[i][j] == 0)
                     lattices_back[i][j] = {};
+                    not_sure.insert({i,j});
+                }
                 else {
+                    lattices_front[i][j] = {(Chessboard[i][j])};
                     lattices_back[i][j] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
                     lattices_back[i][j].erase(Chessboard[i][j]);
+                    sure.push_back({i,j});
                 }
             }
         }
@@ -54,7 +63,10 @@ class Sudoku
     //     structrue[2].fill({1, 2, 3, 4, 5, 6, 7, 8, 9});
     // };
 
-    void eliminate_ambiguity(uint32_t i, uint32_t j, int val, function<void(uint32_t, uint32_t)> action) {
+    void eliminate_ambiguity(
+        uint32_t i, uint32_t j, int val, 
+        function<void(uint32_t, uint32_t)> action
+    ) {
             // 同一行其他未填格子减少可能
             for (uint32_t col = 0; col < 9; ++col) {
                 if (col != j && Chessboard[i][col] == 0 
@@ -63,6 +75,8 @@ class Sudoku
                     lattices_front[i][col].erase(val);
                     lattices_back[i][col].insert(val);
                     if(lattices_front[i][col].size() == 1) {
+                        sure.push_back({i,col});
+                        not_sure.erase({i,col});
                         Chessboard[i][col] = *(lattices_front[i][col].begin());
                         action(i,col);
                     }
@@ -76,6 +90,8 @@ class Sudoku
                     lattices_front[row][j].erase(val);
                     lattices_back[row][j].insert(val);
                     if(lattices_front[row][j].size() == 1) {
+                        sure.push_back({row,j});
+                        not_sure.erase({row,j});
                         Chessboard[row][j] = *(lattices_front[row][j].begin());
                         action(row,j);
                     }
@@ -95,6 +111,8 @@ class Sudoku
                         lattices_front[currentRow][currentCol].erase(val);
                         lattices_back[currentRow][currentCol].insert(val);
                         if(lattices_front[currentRow][currentCol].size() == 1) {
+                            sure.push_back({currentRow,currentCol});
+                            not_sure.erase({currentRow,currentCol});
                             Chessboard[currentRow][currentCol] = *(lattices_front[currentRow][currentCol].begin());
                             action(currentRow,currentCol);
                         }
@@ -105,20 +123,15 @@ class Sudoku
 
 public:
     Sudoku(chess<int> _Chessboard) : Chessboard(_Chessboard) {
-        start_state_to_lattices_front();
-        start_state_to_lattices_back();
+        start_state();
         //start_state_to_chess();
     };
 
     void method_1_WeiYu() {
         queue<position> qu;
 
-        for (uint32_t i = 0; i < 9; i++) {
-            for (uint32_t j = 0; j < 9; j++) {
-                if (lattices_front[i][j].size() == 1) {
-                    qu.push({i,j});
-                }
-            }
+        for (int i = remember_sure; i < sure.size(); ++i) {
+            qu.push(sure[i]);
         }
 
         while(qu.size()) {
@@ -126,99 +139,117 @@ public:
             int val = Chessboard[i][j];
             qu.pop();
 
-            eliminate_ambiguity(i, j, val, [&qu](uint32_t x,uint32_t y){ qu.push({x,y});});
+            eliminate_ambiguity(
+                i, j, val, 
+                [&qu,this](uint32_t x,uint32_t y){ 
+                    qu.push({x,y}); 
+                }
+            );
         }
+        remember_sure = sure.size();
     }
     
     void method_2_PaiChu() {
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                if (Chessboard[i][j] != 0) {
-                    continue; // 跳过已填的格子
+        for (auto [i, j]: not_sure) {
+            if (Chessboard[i][j] != 0) {
+                continue; // 跳过已填的格子
+            }
+
+            // 检查每个可能的数字
+            for (int num : lattices_front[i][j]) {
+                bool canFill = false;
+
+                // 检查行：同一行其他格子的排除集是否都包含 num
+                bool rowValid = true;
+                for (int col = 0; col < 9; ++col) {
+                    if (col == j) continue;
+                    if (lattices_back[i][col].find(num) == lattices_back[i][col].end()) {
+                        //伙伴格子必不可能的数没有该数
+                        rowValid = false;
+                        break;
+                    }
                 }
-    
-                // 检查每个可能的数字
-                for (int num : lattices_front[i][j]) {
-                    bool canFill = false;
-    
-                    // 检查行：同一行其他格子的排除集是否都包含 num
-                    bool rowValid = true;
-                    for (int col = 0; col < 9; ++col) {
-                        if (col == j) continue;
-                        if (lattices_back[i][col].find(num) == lattices_back[i][col].end()) {
-                            //伙伴格子必不可能的数没有该数
-                            rowValid = false;
+                if (rowValid) {
+                    canFill = true;
+                }
+
+                // 检查列：同一列其他格子的排除集是否都包含 num
+                bool colValid = true;
+                if (!canFill) { // 若行已满足，无需再检查
+                    for (int row = 0; row < 9; ++row) {
+                        if (row == i) continue;
+                        if (lattices_back[row][j].find(num) == lattices_back[row][j].end()) {
+                            colValid = false;
                             break;
                         }
                     }
-                    if (rowValid) {
+                    if (colValid) {
                         canFill = true;
                     }
-    
-                    // 检查列：同一列其他格子的排除集是否都包含 num
-                    bool colValid = true;
-                    if (!canFill) { // 若行已满足，无需再检查
-                        for (int row = 0; row < 9; ++row) {
-                            if (row == i) continue;
-                            if (lattices_back[row][j].find(num) == lattices_back[row][j].end()) {
-                                colValid = false;
-                                break;
+                }
+
+                // 检查宫：同一宫其他格子的排除集是否都包含 num
+                bool blockValid = true;
+                if (!canFill) { // 若行或列已满足，无需再检查
+                    int blockRow = (i / 3) * 3;
+                    int blockCol = (j / 3) * 3;
+                    for (int x = blockRow; x < blockRow + 3; ++x) {
+                        for (int y = blockCol; y < blockCol + 3; ++y) {
+                            if (x == i && y == j) continue;
+                            if (lattices_back[x][y].find(num) == lattices_back[x][y].end()) {
+                                blockValid = false;
+                                goto blockCheckEnd; // 使用 goto 跳出多重循环
                             }
                         }
-                        if (colValid) {
-                            canFill = true;
-                        }
                     }
-    
-                    // 检查宫：同一宫其他格子的排除集是否都包含 num
-                    bool blockValid = true;
-                    if (!canFill) { // 若行或列已满足，无需再检查
-                        int blockRow = (i / 3) * 3;
-                        int blockCol = (j / 3) * 3;
-                        for (int x = blockRow; x < blockRow + 3; ++x) {
-                            for (int y = blockCol; y < blockCol + 3; ++y) {
-                                if (x == i && y == j) continue;
-                                if (lattices_back[x][y].find(num) == lattices_back[x][y].end()) {
-                                    blockValid = false;
-                                    goto blockCheckEnd; // 使用 goto 跳出多重循环
-                                }
-                            }
-                        }
-                        blockCheckEnd:
-                        if (blockValid) {
-                            canFill = true;
-                        }
+                    blockCheckEnd:
+                    if (blockValid) {
+                        canFill = true;
                     }
-    
-                    // 若满足条件，填入数字并更新排除集
-                    if (canFill) {
-                        Chessboard[i][j] = num;
-    
-                        eliminate_ambiguity(i, j, num, [](uint32_t x, uint32_t y) {;});
-    
-                        return ;
-                    }
+                }
+
+                // 若满足条件，填入数字并更新排除集
+                if (canFill) {
+                    Chessboard[i][j] = num;
+
+                    eliminate_ambiguity(
+                        i, j, num, 
+                        [this](uint32_t x, uint32_t y) {;}
+                    );
+
+                    break;
                 }
             }
         }
     }
 
     void fillChessboard() {
-        method_1_WeiYu();
-        method_2_PaiChu();
+        while(remember_sure != sure.size()) {
+            method_1_WeiYu();
+            //showChess();
+            method_2_PaiChu();
+            //showChess();
+        }
     }
 
     void showChess() {
+        cout << endl;
         for (uint32_t i = 0; i < 9; i++) {
             if(i && i%3 == 0)
                 cout << endl;
             for (uint32_t j = 0; j < 9; j++) {
                 if(j && j%3 == 0)
                     cout << "   ";
-                cout << Chessboard[i][j] << " ";
+                if(!Chessboard[i][j]) {
+                    cout << "\x1b[38;5;255m" << Chessboard[i][j] << "\x1b[0m" << " ";
+                }
+                else {
+                    cout << "\x1b[38;5;0m" << Chessboard[i][j] << "\x1b[0m" << " ";
+                }
             }
             cout << endl;
         }
+        cout << "------ ------ ------ ------";
     };
 
     void showAmbiguity() {
