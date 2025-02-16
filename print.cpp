0181a4c8646960f0b3f5f990d18e3e6cf98531d6
Sunwenjie <941571283@qq.com>
2025-02-16 15:06:42 +0800

    prepare for more level

--- print.cpp
+++ print.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    int n = 16;
-    int sqrt_n = 4;
+    int n = 25;
+    int sqrt_n = (int)sqrt(25);
     for(int i = 0; i < n; ++i) {
         if(i && i % sqrt_n == 0)
             cout << endl;
-        cout << "\t" << "array<int,16>{";
+        cout << "\t" << "array<int," << n << ">{";
         for(int j = 0; j < n; ++j) {
             if(j && j % sqrt_n == 0)
                 cout << "  ";
