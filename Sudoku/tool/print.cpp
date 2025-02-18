084d3e56002fdbc352081e13ac616856a6d3ff6e
Sunwenjie <941571283@qq.com>
2025-02-18 11:48:15 +0800

    finish base method

---
+++ Sudoku/tool/print.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n = 9;
+    int sqrt_n = (int)sqrt(n);
+    for(int i = 0; i < n; ++i) {
+        if(i && i % sqrt_n == 0)
+            cout << endl;
+        cout << "\t" << "array<int," << n << ">{";
+        for(int j = 0; j < n; ++j) {
+            if(j && j % sqrt_n == 0)
+                cout << "  ";
+            cout << "  0,";
+        }
+        cout << "}," << endl;
+    }
+}
